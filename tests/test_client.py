8b4e36e869ed0fc0a54ae96edb1485ce20d5c63a
Niels Hoppe <mail@nielshoppe.de>
2024-08-18 13:05:31 +0200

    Progress on #1: Convert to Uplink

--- tests/test_client.py
+++ tests/test_client.py
@@ -1,59 +1,84 @@
 import unittest
 import os
 
-from wdsf_api.client import Client
-from wdsf_api.types import Competition, Official, Participant, Person
+from wdsf_api.client import WdsfApi
+# from wdsf_api.types import Competition, Official, Participant, Person
 
-class TestClient(unittest.TestCase):
+class TestWdsfApi(unittest.TestCase):
 
     def setUp(self) -> None:
 
-        self.client = Client(
-            os.getenv('WDSF_API_USERNAME'),
-            os.getenv('WDSF_API_PASSWORD'),
-            raise_for_status=True
+        self.client = WdsfApi(
+            'production',
+            auth=(os.getenv('WDSF_API_USERNAME'), os.getenv('WDSF_API_PASSWORD'))
             )
     
     def test_get_competitions(self):
-        response, data = self.client.get_competitions()
-        self.fail('Test incomplete.')
+        data = self.client.get_competitions()
+        self.assertIsInstance(data, list)
 
     def test_get_competition(self):
         # response, data = self.client.get_competition('61155') # BDF 2024 Sen I
-        response, data = self.client.get_competition('61243') # GOC 2024 Sen I
-        self.assertIsInstance(data, Competition)
+        data = self.client.get_competition('61243') # GOC 2024 Sen I
+        print(data)
+        self.fail('Test incomplete.')
 
     def test_get_participants(self):
-        response, data = self.client.get_participants('61243') # GOC 2024 Sen I
-        self.fail('Test incomplete.')
+        data = self.client.get_participants('61243') # GOC 2024 Sen I
+        self.assertIsInstance(data, list)
 
     def test_get_participant(self):
-        _, data = self.client.get_participant('2223702') # Niels Hoppe - Reenste Seidenberg @ GOC 2024 Sen I
-        self.assertIsInstance(data, Participant)
+        data = self.client.get_participant('2223702') # Niels Hoppe - Reenste Seidenberg @ GOC 2024 Sen I
+        print(data)
+        self.fail('Test incomplete.')
     
     def test_get_officials(self):
-        response, data = self.client.get_participants('61243') # GOC 2024 Sen I
-        self.fail('Test incomplete.')
+        data = self.client.get_participants('61243') # GOC 2024 Sen I
+        self.assertIsInstance(data, list)
 
     def test_get_official(self):
-        _, data = self.client.get_official('298565') # Jeffrey Van Meerkerk
-        self.assertIsInstance(data, Official)
+        data = self.client.get_official('298565') # Jeffrey Van Meerkerk
+        print(data)
+        self.fail('Test incomplete.')
 
     def test_get_couples(self):
-        self.fail('Not implemented.')
+        data = self.client.get_couples()
+        self.assertIsInstance(data, list)
 
     def test_get_couple(self):
-        self.fail('Not implemented.')
+        data = self.client.get_couple('coupleID')
+        self.fail('Test incomplete.')
 
     def test_get_teams(self):
-        self.fail('Not implemented.')
+        data = self.client.get_teams()
+        self.assertIsInstance(data, list)
     
     def test_get_team(self):
-        self.fail('Not implemented.')
+        data = self.client.get_team('teamID')
+        self.fail('Test incomplete.')
 
     def test_get_persons(self):
-        self.fail('Not implemented.')
+        data = self.client.get_persons()
+        self.assertIsInstance(data, list)
 
     def test_get_person(self):
-        response, data = self.client.get_person('10069226') # Niels Hoppe
-        self.assertIsInstance(data, Person)
+        data = self.client.get_person('10069226') # Niels Hoppe
+        print(data)
+        self.fail('Test incomplete.')
+
+    def test_get_ranking(self):
+        data = self.client.get_ranking()
+        self.fail('Test incomplete.')
+
+    def test_get_countries(self):
+        data = self.client.get_countries()
+        self.assertIsInstance(data, list)
+
+    def test_get_age(self):
+        data = self.client.get_age()
+        print(data)
+        self.fail('Test incomplete.')
+
+    def test_check_for_competition(self):
+        data = self.client.check_for_competition('MIN1', 'MIN2', 'competitionId')
+        self.fail('Test incomplete.')
--- wdsf_api/client.py
+++ wdsf_api/client.py
@@ -1,108 +1,200 @@
 from collections import namedtuple
+from uplink import Consumer, get, headers, Path, Query, QueryMap, returns
 
 from wdsf_api.session import Session
 from wdsf_api.parser import Parser
 
 ApiResponse = namedtuple('ApiResponse', ['response', 'data'])
 
-class Client:
+@headers({
+    "Accept": "application/json",
+})
+class WdsfApi(Consumer):
+    '''A Python Client for the WDSF API.'''
 
-    def __init__(self, username, password, **kwargs) -> None:
+    BASE_URLS = {
+        'staging': 'https://sandbox.worlddancesport.org/api/1/',
+        'production': 'https://services.worlddancesport.org/api/1/'
+    }
 
-        self.base_url = 'https://services.worlddancesport.org/api/1/'
-        self.raise_for_status = kwargs.get('raise_for_status', False)
+    def __init__(self, env="production", **kwargs):
+        super().__init__(self.BASE_URLS[env], **kwargs)
 
-        # Initialize the session
-        self.session = Session()
-
-        # If credentials are provided, pass them to the session
-        if username and password:
-            self.session.init_basic_auth(username, password)
-    
-
-    def get(
-            self,
-            path,
-            params = {},
-            **kwargs
-            ):
+    @returns.json
+    @get('competition')
+    def get_competitions(self, **filter: QueryMap):
+        '''Get a list of all competitions
         
-        response = self.session.get(self.base_url + path, params=params)
+        Use query parameter to filter by
+
+        from [DateTime] : list competitions since and including this date (YYYY/MM/DD)
+        to [DateTime] : list competitions after and including this date (YYYY/MM/DD)
+        modifiedsince [Date] : list competitions that have been modified since this date (YYYY/MM/DDThh:mm:ss)
+        worldranking [bool] : true to list competitions that are included in the world ranking list
+        division [string] : General/Professional
+        status: The status of the competition. Valid values are:PreRegistration, Registering, RegistrationClosed, Processing, Closed, Canceled
+        location: The city name where the competition takes/took place.
+        '''
+
+    @returns.json
+    @get('competition/{competition_id}')
+    def get_competition(self, competition_id: Path):
+        '''Get competition by id.'''
+
+    @returns.json
+    @get('participant')
+    def get_participants(self, competition_id: Query('competitionId')):
+        '''Get participants of a competition.'''
+    
+    @returns.json
+    @get('participant/{participant_id}')
+    def get_participant(self, participant_id: Path):
+        '''Get participant of a competition by id.'''
+    
+    @returns.json
+    @get('official')
+    def get_officials(self, competition_id: Query('competitionId')):
+        '''Get officials of a ccompetition.'''
+
+    @returns.json
+    @get('official/{official_id}')
+    def get_official(self, official_id: Path):
+        '''Get official of a competition by id.'''
+
+    @returns.json
+    @get('couple')
+    def get_couples(self, **filter: QueryMap):
+        '''Get a list of all active couples
         
-        if kwargs.get('raise_for_status', self.raise_for_status):
-            response.raise_for_status()
+        Use query parameter to filter by
         
-        return response
+        name [string] : list only couples where any member's name starts with this filter's value
+        phonetic [bool/optional/default=false] : when true, the name filter is used phonetical instead of litteral.
+        min [list of int] :
+        min,min,min.. : list all couples where the members have any of the MIN given
+        min+min : list all couples where all members have the MIN given (make sure the + is URL encoded!)
+        nameOrMin [string] : list couple having a name or MIN starting with this filter's value
+        ageGroup [string] : list couples of an age group (Adult, Senior I, Senior II, Youth, ...)
+        division [string] : General/Professional
+        status [string] : The couple's status, if not given only active couples will be shown. "Any" will show all.
+        country [string] : The couple's country. Separate each country name by a pipe (|).
+        '''
 
+    @returns.json
+    @get('couple/{couple_id}')
+    def get_couple(self, couple_id: Path):
+        '''Get couple by id'''
 
-    def get_competitions(self, **kwargs):
-        response = self.get(
-            'competition',
-            **kwargs
-            )
-        data = None
-        return ApiResponse(response, data)
+    @returns.json
+    @get('team')
+    def get_teams(self):
+        '''Get a list of all active teams
 
+        Use query parameter to filter by
 
-    def get_competition(self, competition_id, **kwargs):
-        response = self.get('competition/' + competition_id, **kwargs)
-        data = Parser.parse_competition(response.text)
-        return ApiResponse(response, data)
+        name [string] : list only couples where any member's name starts with this filter's value
+        phonetic [bool/optional/default=false] : when true, the name filter is used phonetical instead of litteral.
+        '''
+
+    @returns.json
+    @get('team/{team_id}')
+    def get_team(self, team_id: Path):
+        '''Get team by id.'''
+
+    @returns.json
+    @get('person')
+    def get_persons(self, **filter: QueryMap):
+        '''Get a list of all active persons (athletes/adjudicators/chairman)
+
+        Use query parameter to filter by
+
+        name [string] : list all persons having a name starting with this filter's value. Separate name und surname with a comma(,). The order is not relevant.
+        phonetic [bool/optional/default=false] : when true, the name filter is used phonetical instead of litteral.
+        min [int] : list person with this MIN (1xxxxxxx can be omitted)
+        nameOrMin [string] : list persons having a name or MIN starting with this filter's value
+        ageGroup [string] : list persons of an age group (Adult, Senior I, Senior II, Youth, ...)
+        division [string] : General/Professional
+        type [string,string,...] : list persons of a certain type (Athlete, Adjudicator, Chairman)
+        status [string] : list of the person's license status (Active, Retired, Expired, Suspended, Revoked).
+        '''
+
+    @returns.json
+    @get('person/{min}')
+    def get_person(self, min: Path):
+        '''Get person by MIN'''
     
+    @returns.json
+    @get('ranking')
+    def get_ranking(self, **filter: QueryMap):
+        '''Get the world ranking list
 
-    def get_participants(self, competition_id, **kwargs):
-        response = self.get(
-            'participant',
-            params={ 'competitionId': competition_id },
-            **kwargs
-            )
-        data = None
-        return ApiResponse(response, data)
+        Use query paramter to filter by
+
+        ageGroup [string] : the age group (Adult, Senior I, Senior II, Youth, ...)
+        discipline [string] : the discipline (Latin, Standard, Ten Dance)
+        division [string] : the division (General, Professional)
+        form [string] : the dance form (not used yet)
+        gender [string] : the gender (Male, Female)
+        date [Date/optional] : The date of the ranking list (YYYY/MM/DD)
+        limit [int/optional] : Provide only the top x entries
+        '''
     
+    @returns.json
+    @get('country')
+    def get_countries(self):
+        '''Get a list of allowed country names.'''
 
-    def get_participant(self, participant_id, **kwargs):
-        response = self.get('participant/' + participant_id, **kwargs)
-        data = Parser.parse_participant(response.text)
-        return ApiResponse(response, data)
+    @returns.json
+    @get('age')
+    def get_age(self):
+        '''Get a list of age restrictions'''
     
+    @returns.json
+    @get('age/checkforcompetition/{min1},{min2}/{competition_id}')
+    def check_for_competition(self, min1: Path, min2: Path, competition_id: Path):
+        '''Check if a couple is allowed to take part in a competition by their age group.
 
-    def get_officials(self, competition_id, **kwargs):
-        response = self.get(
-            'official',
-            params={ 'competitionId': competition_id },
-            **kwargs
-            )
-        data = None
-        return ApiResponse(response, data)
+        The couple is defined by their MINs.
+        This also works for competitions in years other than the current.'''
 
 
-    def get_official(self, official_id, **kwargs):
-        response = self.get('official/' + official_id, **kwargs)
-        data = Parser.parse_official(response.text)
-        return ApiResponse(response, data)
+'''
+from [DateTime] : list competitions since and including this date (YYYY/MM/DD)
+to [DateTime] : list competitions after and including this date (YYYY/MM/DD)
+modifiedsince [Date] : list competitions that have been modified since this date (YYYY/MM/DDThh:mm:ss)
+worldranking [bool] : true to list competitions that are included in the world ranking list
+division [string] : General/Professional
+status: The status of the competition. Valid values are:PreRegistration, Registering, RegistrationClosed, Processing, Closed, Canceled
+location: The city name where the competition takes/took place.
 
-    
-    def get_couples(self, **kwargs):
-        raise('Not implemented.')
+name [string] : list only couples where any member's name starts with this filter's value
+phonetic [bool/optional/default=false] : when true, the name filter is used phonetical instead of litteral.
+min [list of int] :
+min,min,min.. : list all couples where the members have any of the MIN given
+min+min : list all couples where all members have the MIN given (make sure the + is URL encoded!)
+nameOrMin [string] : list couple having a name or MIN starting with this filter's value
+ageGroup [string] : list couples of an age group (Adult, Senior I, Senior II, Youth, ...)
+division [string] : General/Professional
+status [string] : The couple's status, if not given only active couples will be shown. "Any" will show all.
+country [string] : The couple's country. Separate each country name by a pipe (|).
 
+name [string] : list only couples where any member's name starts with this filter's value
+phonetic [bool/optional/default=false] : when true, the name filter is used phonetical instead of litteral.
 
-    def get_couple(self, couple_id, **kwargs):
-        raise('Not implemented.')
+name [string] : list all persons having a name starting with this filter's value. Separate name und surname with a comma(,). The order is not relevant.
+phonetic [bool/optional/default=false] : when true, the name filter is used phonetical instead of litteral.
+min [int] : list person with this MIN (1xxxxxxx can be omitted)
+nameOrMin [string] : list persons having a name or MIN starting with this filter's value
+ageGroup [string] : list persons of an age group (Adult, Senior I, Senior II, Youth, ...)
+division [string] : General/Professional
+type [string,string,...] : list persons of a certain type (Athlete, Adjudicator, Chairman)
+status [string] : list of the person's license status (Active, Retired, Expired, Suspended, Revoked).
 
-
-    def get_teams(self, **kwargs):
-        raise('Not implemented.')
-
-
-    def get_team(self, team_id, **kwargs):
-        raise('Not implemented.')
-
-
-    def get_persons(self, **kwargs):
-        raise('Not implemented.')
-
-
-    def get_person(self, min, **kwargs):
-        response = self.get('person/' + min, **kwargs)
-        data = Parser.parse_person(response.text)
-        return ApiResponse(response, data)
+ageGroup [string] : the age group (Adult, Senior I, Senior II, Youth, ...)
+discipline [string] : the discipline (Latin, Standard, Ten Dance)
+division [string] : the division (General, Professional)
+form [string] : the dance form (not used yet)
+gender [string] : the gender (Male, Female)
+date [Date/optional] : The date of the ranking list (YYYY/MM/DD)
+limit [int/optional] : Provide only the top x entries
+'''
